6f2c5ae4b4d10caee3d6e2c42816a7f603686225
khumbulani.ndlovu <khumbulani.ndlovu@fdmgrouop.com>
2025-12-25 14:05:23 -0600

    add login form

--- reactive-forms-samples/src/app/components/login-form/login-form.component.ts
+++ reactive-forms-samples/src/app/components/login-form/login-form.component.ts
@@ -1,23 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, ReactiveFormsModule} from '@angular/forms'
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   imports: [ReactiveFormsModule],
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.css'
+  styleUrl: './login-form.component.css',
 })
-export class LoginFormComponent implements OnInit{
- 
+export class LoginFormComponent {
 
-  name = new FormControl('');
-  currentName : string  ='';
+  userForm = new FormGroup({
+    firstname: new FormControl('', Validators.required),
+    lastname: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+  });
 
-   ngOnInit(): void {
-   this.name.valueChanges.subscribe(value =>{
-    this.currentName = value ?? '';
-
-   });
-  }
-
+  currentName: string = '';
+  test: Function | null = null;
+  
 }
